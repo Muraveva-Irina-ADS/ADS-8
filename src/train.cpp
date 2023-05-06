5754259f527a19258eeee4e9f21e90e3658efc75
Muraveva-Irina-ADS <124904325+Muraveva-Irina-ADS@users.noreply.github.com>
2023-05-06 12:46:13 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,45 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+Train::Train() {
+  first = nullptr;
+  countOp = 0;
+}
+void Train::addCage(bool light) {
+  Cage* p = nullptr;
+  if (first == nullptr) {
+    first = new Cage;
+    first->next = first, first->prev = first;
+    first->light = light;
+  } else {
+    p = new Cage;
+    p->next = first, p->prev = first->prev;
+    p->light = light;
+    first->prev = p;
+    p->prev->next = p;
+  }
+}
+int Train::getLength() {
+  Cage* ptr = first;
+  ptr->light = 1;
+  int k = 1;
+  while (ptr) {
+    ptr = ptr->next;
+    if (ptr->light == 0) {
+      k++;
+      countOp++;
+    }
+    if (ptr->light == 1) {
+      ptr->light = 0;
+      for (int i = k; i > 0; i--)
+        ptr = ptr->prev;
+      countOp += k + 1;
+      if (ptr->light == 0)
+        return  k;
+      k = 1;
+    }
+  }
+}
+int Train::getOpCount() {
+  if (countOp == -1) countOp = getLength();
+  return countOp;
+}
